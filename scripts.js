1673921fb77adb25389f402948bee6fe81e3546e
Gabriel <gabrielvdcsilva@gmail.com>
2024-08-20 22:51:24 -0300

    Implements details

--- scripts.js
+++ scripts.js
@@ -2,11 +2,17 @@ const button = document.querySelector('.addButton');
 const input = document.querySelector('.inputList')
 const completeList = document.querySelector('.listTasks')
 const finishedList = document.querySelector('.finishedTasks')
+const error = document.querySelector('.error')
 
 let itemsList = []
 
 
 function addingTask(){
+
+    if(!validateInput()){
+
+        return;
+    }
 
     itemsList.push({
         task: input.value,
@@ -31,7 +37,7 @@ function showTasks(){
 
             finishedTasks += `
                     <li class="task">
-                    <p>${fields.task}</p> 
+                    <p class="${taskComplete && "done"}">${fields.task}</p> 
                     <i class="bi bi-trash" style="color:red; cursor: pointer;" onclick="deleteItem(${index})"></i> 
                     </li>
             `
@@ -49,6 +55,14 @@ function showTasks(){
 
     completeList.innerHTML = todoTasks;
     finishedList.innerHTML = finishedTasks;
+
+    const finishedTitle = document.querySelector('.finishedTitle');
+    finishedTitle.style.display = finishedTasks.trim() === '' ? 'none' : 'block';
+
+    const loading = document.querySelector('.loading');
+    loading.style.display = todoTasks.length < 1 ? 'block' : 'none'
+
+    localStorage.setItem('List', JSON.stringify(itemsList))
 }
 
 function taskComplete(index){
@@ -63,9 +77,36 @@ function deleteItem(index) {
 }
 
 function validateInput(){
-    if(input ==='') {
-        input.classList.add('error')
+    if(input.value.trim() ==='') {
+        input.classList.add('inputList-error')
+        error.textContent = 'O campo de tarefa não pode estar vazio!'
+        error.classList.add('error-message')
+        return false
+    }else if(input.value.length < 3){
+        input.classList.add('inputList-error')
+        error.textContent = 'Mínimo de 3 caracteres!'
+        error.classList.add('error-message')
+        return false
+    }else{
+        return true
     }
 }
+
+input.addEventListener('input', ()=>{
+    if(input.value.trim() != ''){
+        input.classList.remove('inputList-error')
+        error.classList.remove('error-message')
+    }
+})
+
+function loadTasks() {
+    const tasksLocalStorage = localStorage.getItem('List')
+    if(tasksLocalStorage){
+
+        itemsList = JSON.parse(tasksLocalStorage)
+    }
+    showTasks()
+}
+loadTasks()
 
 button.addEventListener('click', addingTask)
